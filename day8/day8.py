9e326c63f18a39264c771076f4d80f8c87af521d
Mederic de Verdilhac <mederic.deverdilhac@orange.com>
2024-10-26 22:22:28 +0200

    Update

---
+++ day8/day8.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import os
+from typing import List
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def read_input(path: str) -> List[str]:
+    with open(path, 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def run1(lines: List[str]):
+    printed_char_count = 0
+    memory_char_count = 0
+    for line in lines:
+        printed_char_count += len(line)
+        i = 1
+        memory = 0
+        while i < len(line) -1:
+            if line[i] == "\\" and line[i+1] == "x":
+                memory += 1
+                i += 4
+                continue
+            if line[i] == "\\":
+                memory += 1
+                i += 2
+                continue
+            else:
+                memory += 1
+                i += 1
+        memory_char_count += memory
+
+    return printed_char_count, memory_char_count
+
+
+
+#INPUT = f"{SCRIPT_DIR}/input_test.txt"
+INPUT = f"{SCRIPT_DIR}/input.txt"
+data = read_input(INPUT)
+printed_char_count, memory_char_count = run1(data)
+print("Result1 = ", printed_char_count - memory_char_count)
+
+# =========================================
+
+
+
+def run2(lines: List[str]):
+    total_char_count = 0
+    for line in lines:
+        i = 0
+        char_count = 2
+        while i < len(line):
+            if line[i] in ["\\", "\"", "\'"]:
+                char_count += 2
+                i += 1
+            else:
+                char_count += 1
+                i += 1
+        total_char_count += char_count
+    return total_char_count
+
+encoded = run2(data)
+print("Result2 = ", encoded - printed_char_count)
---
+++ day9/day9.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+from typing import List
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def read_input(path: str) -> List[str]:
+    with open(path, 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parse(lines):
+    distances = {}
+    for line in lines:
+        match line.split(" "):
+            case [city1, "to", city2, "=", distance]:
+                distances.setdefault(city1,{})[city2] = int(distance)
+                distances.setdefault(city2,{})[city1] = int(distance)
+            case _:
+                print("ERROR", line)
+                sys.exit(1)
+    return distances
+
+
+def permutations(elements):
+    if len(elements) <= 1:
+        yield elements
+        return
+    for perm in permutations(elements[1:]):
+        for i in range(len(elements)):
+            yield perm[:i] + elements[0:1] + perm[i:]
+
+
+def run(distances):
+    path_proposals = list(permutations(list(distances.keys())))
+    possible_path_lenghs = []
+    for path in path_proposals:
+        path_lengh = 0
+        for i in range(len(path)-1):
+            path_lengh += distances[path[i]][path[i+1]]
+        # print(path, path_lengh)
+        possible_path_lenghs.append(path_lengh)
+    return min(possible_path_lenghs), max(possible_path_lenghs)
+
+
+#INPUT = f"{SCRIPT_DIR}/input_test.txt"
+INPUT = f"{SCRIPT_DIR}/input.txt"
+distances = parse(read_input(INPUT))
+result1, result2 = run(distances)
+print("Result1 = ", result1)
+print("Result2 = ", result2)
